facde0c0ecb2c083014e5c23b62f4b2fe43f157c
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-04-10 13:25:03 +0700

    :sparkles: load detail place

--- src/screens/listplace/DetailListPlace.js
+++ src/screens/listplace/DetailListPlace.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 class DetailListPlaceScreen extends React.Component {
 
@@ -13,11 +13,33 @@ class DetailListPlaceScreen extends React.Component {
     render () {
         return (
             <View>
-                <Text>Detail List Place {this.state.place.nama}</Text>
+                <Image source={{uri:this.state.place.gambar}} style={styles.containerImage} />
+                <Text style={styles.textName}>{this.state.place.nama}</Text>
+                <Text style={styles.textDescription}>{this.state.place.deskripsi}</Text>
             </View>
         )
     }
 
 }
+
+const styles = StyleSheet.create({
+    containerImage: {
+        width: "100%",
+        height: 250
+    },
+    textName: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginTop: 8,
+        marginLeft: 8,
+        marginRight: 8
+    },
+    textDescription: {
+        fontSize: 16,
+        marginTop: 8,
+        marginLeft: 8,
+        marginRight: 8
+    }
+});
 
 export default DetailListPlaceScreen;
