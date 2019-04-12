865e175747bf3f8d98e2edb3fd5bb96275ca20f1
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-04-12 10:17:06 +0700

    :sparkles: screens thanks

--- src/screens/about/Thanks.js
+++ src/screens/about/Thanks.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import AppTextRegular from '../../components/AppTextRegular';
+
 class ThanksScreen extends React.Component {
     render () {
         return (
-            <View>
-                <Text>Thanks Screen</Text>
+            <View style={styles.container}>
+                <AppTextRegular style={styles.textThanks}>terima kasih kepada blog-blog maupun perorangan yang fotonya dimuat dalam aplikasi ini,
+                dan tidak bisa disebutkan satu persatu (kalian luar biasa...)</AppTextRegular>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingLeft: 16,
+        paddingRight: 16,
+        paddingTop: 16
+    },
+    textThanks: {
+        fontSize: 17
+    }
+});
 
 export default ThanksScreen;
