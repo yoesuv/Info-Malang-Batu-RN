96c3991d4b794e2d318c58d5a0a833d5b0265079
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-04-12 15:11:31 +0700

    :lipstick: change icon bottom navigation focused

--- App.js
+++ App.js
@@ -14,9 +14,13 @@ import AboutScreen from './src/screens/about/About';
 import Icon from './src/components/Icon';
 
 import iconList from './src/images/ic_menu_list.png';
+import iconListSelected from './src/images/ic_menu_list_selected.png';
 import iconGallery from './src/images/ic_menu_gallery.png';
+import iconGallerySelected from './src/images/ic_menu_gallery_selected.png';
 import iconMaps from './src/images/ic_menu_maps.png';
+import iconMapsSelected from './src/images/ic_menu_maps_selected.png';
 import iconOther from './src/images/ic_menu_other.png';
+import iconOtherSelected from './src/images/ic_menu_other_selected.png';
 
 import { THEME_COLOR } from './src/data/Constants';
 
@@ -144,12 +148,24 @@ let TabNavigator = createBottomTabNavigator(
             tabBarIcon: ({ focused, horizontal, tintColor }) => {
                 const { routeName } = navigation.state;
                 if (routeName === 'List') {
+                    if (focused) {
+                        return <Icon image={iconListSelected}/>;
+                    }
                     return <Icon image={iconList}/>;
                 } else if (routeName === 'Gallery') {
+                    if (focused) {
+                        return <Icon image={iconGallerySelected}/>;
+                    }
                     return <Icon image={iconGallery}/>;
                 } else if (routeName === 'Map') {
+                    if (focused) {
+                        return <Icon image={iconMapsSelected}/>;
+                    }
                     return <Icon image={iconMaps}/>;
                 }
+                    if (focused) {
+                        return <Icon image={iconOtherSelected}/>;
+                    }
                     return <Icon image={iconOther}/>;
                 },
         }),
