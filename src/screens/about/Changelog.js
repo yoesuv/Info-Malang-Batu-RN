96e19999d379b3fb3f46530235c9feefd11916e8
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-04-12 14:39:34 +0700

    :sparkles: changelog screens

--- src/screens/about/Changelog.js
+++ src/screens/about/Changelog.js
@@ -1,14 +1,42 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+
+import ItemChangelog from '../../components/ItemChangelog';
 
 class ChangelogScreen extends React.Component {
+
+    state = {
+        changelogs: [{
+            versionName: 'Version 1.0.0',
+            versionInfo: '• initial release'
+        }]
+    }
+
     render () {
         return (
             <View>
-                <Text>Changelog Screen</Text>
+            <FlatList
+                style={styles.containerFlatList}
+                data={this.state.changelogs}
+                renderItem={
+                    ({item, index}) => (
+                        <ItemChangelog
+                            versionName={item.versionName}
+                            versionInfo={item.versionInfo}/>
+                    )
+                }
+                keyExtractor={(item, index) => index.toString()}
+            />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    containerFlatList: {
+        paddingLeft: 16,
+        paddingRight: 16
+    }
+});
 
 export default ChangelogScreen;
