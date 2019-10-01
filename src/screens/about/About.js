989fe1704c24c94c2366432f026d445242b0585e
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-10-01 16:39:17 +0700

    :arrow_up: :tada: update react native 0.60+

--- src/screens/about/About.js
+++ src/screens/about/About.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { View, StyleSheet } from "react-native";
-import { IndicatorViewPager, PagerTitleIndicator } from 'rn-viewpager';
 
 import { THEME_COLOR } from '../../data/Colors';
 
@@ -11,40 +10,10 @@ import LibrariesScreen from './Libraries';
 
 class AboutScreen extends Component {
 
-    renderTab() {
-         return (
-             <PagerTitleIndicator
-                style={styles.indicatorContainer}
-                trackScroll={true}
-                itemTextStyle={styles.indicatorText}
-                selectedItemTextStyle={styles.indicatorSelectedText}
-                selectedBorderStyle={styles.selectedBorderStyle}
-                titles={['Information', 'Changelog','Thanks','Libraries']}
-             />
-         )
-   }
-
     render () {
         return (
             <View style={{flex:1}}>
-                <IndicatorViewPager
-                    style={styles.viewPagerContainer}
-                    initialPage={0}
-                    indicator={this.renderTab()}>
-                        <View>
-                            <InformationScreen />
-                        </View>
-                        <View>
-                            <ChangelogScreen />
-                        </View>
-                        <View>
-                            <ThanksScreen />
-                        </View>
-                        <View>
-                            <LibrariesScreen />
-                        </View>
 
-                </IndicatorViewPager>
             </View>
         )
     }
