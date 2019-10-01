fd4e56fc04338014c41ebbd8a59943fe9fe0d0e9
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-10-01 15:30:30 +0700

    :arrow_up: update react-navigation

--- src/routes/MapRoute.js
+++ src/routes/MapRoute.js
@@ -1,4 +1,4 @@
-import { createStackNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
 import { THEME_COLOR, HEADER_TINT_COLOR } from '../data/Colors';
 import { MyHeader } from './Header';
