1ef53fdb6b7abe940a6f7417acdaa2c31ca8fa85
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-21 16:24:55 +0700

    :fire: remove old files

--- src/routes/Header.js
+++
@@ -1,6 +0,0 @@
-export const MyHeader = {
-    titleStyle: {
-        fontWeight: '200',
-        fontFamily: 'Pacifico'
-    }
-}
--- src/store/actions/index.js
+++
@@ -1,3 +0,0 @@
-export { getListPlace, getGallery, getPins } from './myApplicationData';
-export { uiStartLoadingList, uiStopLoadingList, uiNetworkErrorList } from './uiListPlace';
-export { uiStartLoadingGallery, uiStopLoadingGallery, uiNetworkErrorGallery } from './uiGallery';
