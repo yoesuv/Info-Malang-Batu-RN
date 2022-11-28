3961a55145f29c780dcb8894eec722bf6c8a44ef
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-28 08:11:35 +0700

    :sparkles: detail list place

--- src/screens/detail-list-place.tsx
+++ src/screens/detail-list-place.tsx
@@ -1,8 +1,8 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 import { Text, Appbar } from 'react-native-paper';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StackNavigationProp } from '@react-navigation/stack';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute, RouteProp } from '@react-navigation/native';
 
 import { THEME_COLOR } from '../data/colors';
 import { RootStackParamList } from './root-stack-params';
@@ -11,6 +11,8 @@ type detailPlaceScreenProp = StackNavigationProp<RootStackParamList, 'DetailPlac
 
 export default function DetailListPlaceScreen() {
 
+    const route = useRoute<RouteProp<RootStackParamList, 'DetailPlace'>>();
+    const params = route.params;
     const navigation = useNavigation<detailPlaceScreenProp>();
 
     const pressBack = () => {
@@ -22,7 +24,11 @@ export default function DetailListPlaceScreen() {
             <Appbar.BackAction color='white' onPress={pressBack} />
             <Appbar.Content title="Detail Place" titleStyle={styles.title} />
         </Appbar.Header>
-        <Text style={{ fontSize: 20 }}>Detail Place</Text>
+        <View style={styles.containerImage}>
+            <Image source={{uri: params.gambar}} style={styles.containerImage} />
+        </View>
+        <Text style={styles.textName}>{params.nama}</Text>
+        <Text style={styles.textDescription}>{params.deskripsi}</Text>
     </SafeAreaView>
 }
 
@@ -32,9 +38,27 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         backgroundColor: 'white'
     },
+    containerImage: {
+        width: "100%",
+        height: 250,
+        backgroundColor: "#EEEEEE",
+    },
     title: {
         fontSize: 20,
         fontWeight: 'bold',
         color: 'white',
+    },
+    textName: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginTop: 8,
+        marginLeft: 8,
+        marginRight: 8,
+    },
+    textDescription: {
+        fontSize: 16,
+        marginTop: 8,
+        marginLeft: 8,
+        marginRight: 8,
     }
 })
