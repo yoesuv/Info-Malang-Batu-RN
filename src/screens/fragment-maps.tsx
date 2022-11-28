1947f540e30dcef111a854a110f5125c7c67f49a
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-28 20:38:54 +0700

    :pencil: add google maps

--- src/screens/fragment-maps.tsx
+++ src/screens/fragment-maps.tsx
@@ -1,13 +1,21 @@
 import { StyleSheet, View } from "react-native";
 import { Appbar } from 'react-native-paper';
+import MapView from 'react-native-maps';
 
 import { THEME_COLOR } from "../data/colors";
+import { DEFAULT_LATITUDE, DEFAULT_LONGITUDE } from "../data/constants";
 
 export default function FragmentMaps() {
     return <View style={styles.container}>
         <Appbar.Header mode="small" statusBarHeight={0} style={{ backgroundColor: THEME_COLOR }} >
             <Appbar.Content title="Maps" titleStyle={styles.title} />
         </Appbar.Header>
+        <MapView style={styles.map} initialRegion={{
+            latitude: DEFAULT_LATITUDE,
+            longitude: DEFAULT_LONGITUDE,
+            latitudeDelta: 0.85,
+            longitudeDelta: 0.85,
+        }} />
     </View>
 }
 
@@ -21,5 +29,9 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
         color: 'white',
+    },
+    map: {
+        width: '100%',
+        height: '100%',
     },
 })
