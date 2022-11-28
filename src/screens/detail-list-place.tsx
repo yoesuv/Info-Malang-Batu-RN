ee6c4e54abc713fcc2b50f0d452f66735bb2a555
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-28 07:57:35 +0700

    :pencil: handle app bar back

--- src/screens/detail-list-place.tsx
+++ src/screens/detail-list-place.tsx
@@ -1,12 +1,25 @@
 import { StyleSheet, View } from 'react-native';
 import { Text, Appbar } from 'react-native-paper';
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StackNavigationProp } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
 
 import { THEME_COLOR } from '../data/colors';
+import { RootStackParamList } from './root-stack-params';
+
+type detailPlaceScreenProp = StackNavigationProp<RootStackParamList, 'DetailPlace'>;
 
 export default function DetailListPlaceScreen() {
+
+    const navigation = useNavigation<detailPlaceScreenProp>();
+
+    const pressBack = () => {
+        navigation.pop();
+    }
+
     return <SafeAreaView style={styles.container}>
         <Appbar.Header mode="small" statusBarHeight={0} style={{ backgroundColor: THEME_COLOR }}>
+            <Appbar.BackAction color='white' onPress={pressBack} />
             <Appbar.Content title="Detail Place" titleStyle={styles.title} />
         </Appbar.Header>
         <Text style={{ fontSize: 20 }}>Detail Place</Text>
