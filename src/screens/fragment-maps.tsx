2e916978a37b28f4e520bdf5d31f078f5f6dc114
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-30 05:39:38 +0700

    :pencil: load map view

--- src/screens/fragment-maps.tsx
+++ src/screens/fragment-maps.tsx
@@ -1,27 +1,43 @@
 import { StyleSheet, View } from "react-native";
-import { Appbar } from 'react-native-paper';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { Appbar, ActivityIndicator } from 'react-native-paper';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 import { THEME_COLOR } from "../data/colors";
 import { DEFAULT_LATITUDE, DEFAULT_LONGITUDE } from "../data/constants";
 import { MAP_STYLE } from "../data/maps-style";
+import UsePins from "../services/pin-service";
 
 export default function FragmentMaps() {
+
+    const { data, status } = UsePins();
+
     return <View style={styles.container}>
         <Appbar.Header mode="small" statusBarHeight={0} style={{ backgroundColor: THEME_COLOR }} >
             <Appbar.Content title="Maps" titleStyle={styles.title} />
         </Appbar.Header>
-        <MapView style={styles.map}
-            provider={PROVIDER_GOOGLE}
-            initialRegion={{
-                latitude: DEFAULT_LATITUDE,
-                longitude: DEFAULT_LONGITUDE,
-                latitudeDelta: 0.85,
-                longitudeDelta: 0.85,
-            }}
-            customMapStyle={MAP_STYLE}
-        />
+        { status === 'loading' && <LoadingView /> }
+        { status === 'success' && <MapsPinView/> }
     </View>
+}
+
+const LoadingView = () => {
+    return <View style={styles.contentLoading}>
+        <ActivityIndicator size={'large'} />
+    </View>
+}
+
+const MapsPinView = () => {
+    return <MapView 
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+            latitude: DEFAULT_LATITUDE,
+            longitude: DEFAULT_LONGITUDE,
+            latitudeDelta: 0.85,
+            longitudeDelta: 0.85,
+        }}
+        customMapStyle={MAP_STYLE}
+    />
 }
 
 const styles = StyleSheet.create({
@@ -29,6 +45,10 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         backgroundColor: 'white'
+    },
+    contentLoading: {
+        flex: 1,
+        justifyContent: 'center',
     },
     title: {
         fontSize: 20,
