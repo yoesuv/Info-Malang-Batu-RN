9574fa64246d3fe961fecd0e2fcc196237297660
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-12-02 15:50:21 +0700

    :sparkles: app name & version

--- src/screens/tabs/tab-information.tsx
+++ src/screens/tabs/tab-information.tsx
@@ -1,14 +1,20 @@
 import { StyleSheet, View } from "react-native";
 import { Text } from 'react-native-paper';
+import * as Application from 'expo-application';
+
 import SizedBox from "../../components/sized-box";
 
 export default function TabInformation() {
+
+    const appName = Application.applicationName;
+    const appVersion = Application.nativeApplicationVersion;
+
     return <View style={styles.container}>
         <SizedBox height={10} />
-        <Text style={styles.title}>App Name</Text>
+        <Text style={styles.title}>{ appName }</Text>
         <SizedBox height={5} />
         <Text style={styles.content}>React Native Version</Text>
-        <Text style={styles.content}>1.0.0</Text>
+        <Text style={styles.content}>{ appVersion }</Text>
     </View>
 }
 
