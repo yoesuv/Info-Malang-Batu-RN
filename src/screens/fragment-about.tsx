9f106bb0281878a4fa1a2b99db7425e70b96a872
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-12-02 13:29:40 +0700

    :newspaper: add fragment about

---
+++ src/screens/fragment-about.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, View } from 'react-native';
+import { Appbar } from 'react-native-paper';
+import { THEME_COLOR } from '../data/colors';
+
+export default function FragmentAbout() {
+    return <View style={styles.container}>
+        <Appbar.Header mode="small" statusBarHeight={0} style={{ backgroundColor: THEME_COLOR }}>
+            <Appbar.Content title="About" titleStyle={styles.title} />
+        </Appbar.Header>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'white'
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+})
