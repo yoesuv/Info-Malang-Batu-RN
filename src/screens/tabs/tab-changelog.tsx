2b6039869076529de1773218a9147a6b6854d1b4
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-12-03 13:26:40 +0700

    :sparkles: item changelog

--- src/screens/tabs/tab-changelog.tsx
+++ src/screens/tabs/tab-changelog.tsx
@@ -1,4 +1,6 @@
-import { StyleSheet, View, FlatList, Text } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
+import { Divider } from "react-native-paper";
+import { ItemChangelog } from "../../components/item-changelog";
 
 import { APP_CHANGELOG } from "../../data/constants";
 
@@ -6,8 +8,9 @@ export default function TabChangelog() {
     return <View style={styles.container}>
         <FlatList
             data={APP_CHANGELOG}
-            renderItem={({item, index}) => <Text>{item.versionName}</Text>}
+            renderItem={({item, index}) => <ItemChangelog name={item.versionName} info={item.versionInfo} />}
             keyExtractor={(item, index) => index.toString()}
+            ItemSeparatorComponent={(item, index) => <Divider />}
         />
     </View>
 }
