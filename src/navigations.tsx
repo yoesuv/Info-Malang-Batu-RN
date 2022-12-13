7e0fa396caeb997a849c5a219c7fe619d9f598eb
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-12-13 13:26:50 +0700

    :art: splash background

--- src/navigations.tsx
+++ src/navigations.tsx
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator, NativeStackNavigationOptions } from '@react-navigation/native-stack';
-import { THEME_COLOR_700 } from './data/colors';
+import { THEME_COLOR, THEME_COLOR_700 } from './data/colors';
 import DetailGalleryScreen from './screens/detail-gallery';
 import DetailListPlaceScreen from './screens/detail-list-place';
 import HomeScreen from './screens/home';
@@ -17,7 +17,7 @@ const baseOptions: NativeStackNavigationOptions = {
 const AppNavigation = () => {
     return <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen name='Splash' component={SplashScreen} options={{...baseOptions, statusBarColor: 'white'}} />
+            <Stack.Screen name='Splash' component={SplashScreen} options={{...baseOptions, statusBarColor: THEME_COLOR}} />
             <Stack.Screen name='Home' component={HomeScreen} options={{...baseOptions}} />
             <Stack.Screen name='DetailPlace' component={DetailListPlaceScreen} options={{...baseOptions}} />
             <Stack.Screen name='DetailGallery' component={DetailGalleryScreen} options={{...baseOptions}} />
