1d814b8e9ca35f1f77e4d596fe22cf92a6d08671
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-12-13 13:22:05 +0700

    :pencil: use expo application package

--- src/screens/tabs/tab-information.tsx
+++ src/screens/tabs/tab-information.tsx
@@ -1,16 +1,20 @@
 import { StyleSheet, View } from "react-native";
 import { Text } from 'react-native-paper';
+import * as Application from 'expo-application';
 
 import SizedBox from "../../components/sized-box";
 
 export default function TabInformation() {
 
+    const appName = Application.applicationName;
+    const appVersion = Application.nativeApplicationVersion;
+
     return <View style={styles.container}>
         <SizedBox height={10} />
-        <Text style={styles.title}>Info Malang Batu</Text>
+        <Text style={styles.title}>{ appName }</Text>
         <SizedBox height={5} />
         <Text style={styles.content}>React Native Version</Text>
-        <Text style={styles.content}>2.0.0</Text>
+        <Text style={styles.content}>{ appVersion }</Text>
     </View>
 }
 
